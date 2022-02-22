56fb082722ae9c6cdaf42f543f3321d55e715469
mupsy <amm@muxinsq.com>
2022-02-22 11:49:01 +0800

    Beta version of the new plug-in

---
+++ plugins/development/plugins/plugin-SALT.js
@@ -0,0 +1,290 @@
+/*
+ * Example plugin template
+ */
+
+jspsych.plugins["SALT"] = (function () {
+  'use strict';
+
+  const info = {
+    name: "SALT",
+    parameters: {
+      stimuli: {
+        type: jsPsych.ParameterType.COMPLEX, // This is similar to the quesions of the survey-likert. 
+        array: true,
+        pretty_name: 'Stimuli',
+        nested: {
+          show_start_time: {
+            type: jsPsych.ParameterType.INT,
+            pretty_name: 'Show start time',
+            default: 0
+          },
+          show_end_time: {
+            type: jsPsych.ParameterType.INT,
+            pretty_name: 'Show end time',
+            default: null
+          },
+          v_angle: {
+            type: jsPsych.ParameterType.INT,
+            pretty_name: 'View angle',
+            default: 1
+          },
+          color: {
+            type: jsPsych.ParameterType.STRING,
+            pretty_name: "Color",
+            default: "white"
+          },
+          lineWidth: {
+            type: jsPsych.ParameterType.INT,
+            pretty_name: 'line width',
+            default: 5
+          },
+          bias_angle: {
+            type: jsPsych.ParameterType.INT,
+            pretty_name: 'bias angle',
+            default: 0
+          },
+          obj_type: {
+            type: jsPsych.ParameterType.STRING,
+            pretty_name: "Obj type",
+            default: null
+          },
+          content: {
+            type: jsPsych.ParameterType.STRING,
+            pretty_name: "Content",
+            default: null
+          }
+        }
+      },
+      distance: {
+        type: jsPsych.ParameterType.INT,
+        default: 500
+      },
+      screen: {
+        type: jsPsych.ParameterType.COMPLEX,
+        description: "Screen related information",
+        nested: {
+          pixelW: {
+            type: jsPsych.ParameterType.INT,
+            default: 1440,
+            description: 'Screen width pixels.'
+          },
+          pixelH: {
+            type: jsPsych.ParameterType.INT,
+            default: 900,
+            description: 'Screen height pixels.'
+          },
+          actualW: {
+            type: jsPsych.ParameterType.INT,
+            default: 1440,
+            description: 'Actual screen width size. The unit is mm.'
+          },
+          actualH: {
+            type: jsPsych.ParameterType.INT,
+            default: 1440,
+            description: 'Actual screen height size. The unit is mm.'
+          }
+        }
+      },
+      canvas_width: {
+        type: jsPsych.ParameterType.INT,
+        pretty_name: 'Canvas width',
+        default: window.innerWidth * 0.9
+      },
+      canvas_height: {
+        type: jsPsych.ParameterType.INT,
+        pretty_name: 'Canvas height',
+        default: window.innerHeight * 0.9
+      },
+      background_color: {
+        type: jsPsych.ParameterType.STRING,
+        pretty_name: 'Background color',
+        default: 'grey'
+      },
+      trial_duration: {
+        type: jsPsych.ParameterType.INT,
+        pretty_name: "Trial duration",
+        default: 2000
+      },
+      choices: {
+        type: jsPsych.ParameterType.KEYS,
+        pretty_name: "Choices",
+        default: "ALL_KEYS",
+      },
+      response_start_time: {
+        type: jsPsych.ParameterType.INT,
+        pretty_name: "Response start time",
+        default: 0
+      }
+    }
+  }
+
+  class SALT {
+    constructor(jspsych) {
+      this.jspsych = jspsych;
+    }
+    trial(display_element, trial) {
+      class visual_stimulus {
+        constructor(stim) {
+          Object.assign(this, stim)
+          const keys = Object.keys(this)
+          for (var i = 0; i < keys.length; i++) {
+            if (typeof this[keys[i]] === "function") {
+              // オブジェクト内のfunctionはここで指定する必要がある。そうしないとここで即時に実行されて、その結果が関数名に代入される
+              if (keys[i] === "drawFunc") continue
+              if (keys[i] === "change_attr") continue
+              if (keys[i] === "mask_func") continue
+
+              this[keys[i]] = this[keys[i]].call()
+            }
+          }
+        }
+      }
+
+      class cross_stimulus extends visual_stimulus {
+        constructor(stim) {
+          super(stim);
+        }
+        show() {
+          ctx.beginPath();
+          ctx.lineWidth = this.lineWidth;
+          ctx.strokeStyle = this.color;
+          let pW = Utils.getPixe(trial.distance, this.v_angle, trial.screen.pixelW, trial.screen.actualW),
+            pH = Utils.getPixe(trial.distance, this.v_angle, trial.screen.pixelH, trial.screen.actualH);
+
+          ctx.moveTo(trial.canvas_width / 2, trial.canvas_height / 2 - pW);
+          ctx.lineTo(trial.canvas_width / 2, trial.canvas_height / 2 + pW);
+          ctx.moveTo(trial.canvas_width / 2 - pH, trial.canvas_height / 2);
+          ctx.lineTo(trial.canvas_width / 2 + pH, trial.canvas_height / 2);
+          ctx.stroke();
+        }
+      }
+
+      class image_stimulus extends visual_stimulus {
+        constructor(stim) {
+          super(stim);
+        }
+        show() {
+          let img = new Image();
+          img.src = this.content;
+
+          let pW = Utils.getPixe(
+            trial.distance, this.v_angle, trial.screen.pixelW, trial.screen.actualW, this.bias_angle
+          )
+          let pH = Utils.getPixe(
+            trial.distance, this.v_angle, trial.screen.pixelH, trial.screen.actualH, this.bias_angle
+          )
+          // console.log(this.pW, this.pH);
+          ctx.drawImage(
+            img,
+            0, 0, img.width, img.height,
+            trial.canvas_width / 2 - pW / 2,
+            trial.canvas_height / 2 - Utils.getPixe(
+              trial.distance, this.bias_angle, trial.screen.pixelH, trial.screen.actualH
+            ) - pH / 2,
+            pW, pH
+          );
+
+        }
+      }
+
+      class text_stimulus extends visual_stimulus {
+        constructor(stim) {
+          super(stim);
+        }
+        show() {
+          let pH = Utils.getPixe(
+            trial.distance, this.v_angle, trial.screen.pixelH, trial.screen.actualH, this.bias_angle
+          )
+          ctx.font = `${pH}px Arual`
+          ctx.fillStyle = this.color;
+          ctx.textAlign = "center";
+          ctx.textBaseline = "middle"
+
+          ctx.fillText(
+            this.content,
+            trial.canvas_width / 2,
+            trial.canvas_height / 2 + Utils.getPixe(
+              trial.distance, this.bias_angle, trial.screen.pixelH, trial.screen.actualH
+            ) + pH / 4
+          );
+        }
+      }
+      let cv = document.createElement("canvas");
+      let ctx = cv.getContext("2d");
+
+      cv.height = trial.canvas_height;
+      cv.width = trial.canvas_width;
+      cv.style.backgroundColor = trial.background_color;
+      display_element.appendChild(cv);
+
+      const stimulus = {
+        text: text_stimulus,
+        image: image_stimulus,
+        cross: cross_stimulus
+      }
+      let oop_stim = [];
+      for (let i of trial.stimuli) {
+        if (!i.obj_type) {
+          alert('You have missed to specify the obj_type property in the ' + (i + 1) + 'th object.');
+          return
+        }
+        oop_stim.push(new stimulus[i.obj_type](i));
+      }
+
+      function step() {
+        ctx.clearRect(0, 0, cv.width, cv.height);
+        let elapsedTime = performance.now() - start_time;
+
+        for (let i of oop_stim) {
+          if (elapsedTime > i.show_start_time && (!i.show_end_time || elapsedTime <= i.show_end_time)) {
+            i.show()
+          }
+        }
+
+      }
+
+      // function to end trial when it is time
+      var end_trial = (info) => {
+
+        // kill any remaining setTimeout handlers
+        this.jspsych.pluginAPI.clearAllTimeouts();
+
+        // kill keyboard listeners
+        if (typeof keyboardListener !== 'undefined') {
+          this.jspsych.pluginAPI.cancelKeyboardResponse(keyboardListener);
+        }
+        clearInterval(intervalID);
+        // gather the data to store for the trial
+        var trial_data = {
+          rt: info ? info.rt : null,
+          response: info ? info.key : null
+        };
+
+        // clear the display
+        display_element.innerHTML = '';
+
+        // move on to the next trial
+        this.jspsych.finishTrial(trial_data);
+      };
+
+      const start_time = performance.now();
+      var intervalID = setInterval(step, 1);
+      this.jspsych.pluginAPI.setTimeout(() => {
+        if (trial.choices != "NO_KEYS") {
+          var keyboardListener = this.jspsych.pluginAPI.getKeyboardResponse({
+            callback_function: end_trial,
+            valid_responses: trial.choices,
+            rt_method: "performance",
+            persist: false,
+            allow_held_key: false,
+          });
+        }
+      }, trial.response_start_time);
+      if (trial.trial_duration) {
+        jspsych.pluginAPI.setTimeout(end_trial, trial.trial_duration);
+      }
+    };
+  }
+  SALT.info = info;
+  return SALT;
+})();
---
+++ plugins/development/plugins/plugin-get-screen-size.js
@@ -0,0 +1,142 @@
+jspsych.plugins["get-screen-size"] = (function () {
+    'use strict';
+
+    const info = {
+        name: "get-screen-size",
+        parameters: {
+            /** The height of the item to be measured, the unit is mm. */
+            item_height: {
+                type: jsPsych.ParameterType.INT,
+                pretty_name: "Item height",
+                default: 54,
+            },
+            /** The width of the item to be measured, the unit is mm. */
+            item_width: {
+                type: jsPsych.ParameterType.INT,
+                pretty_name: "Item width",
+                default: 85.5,
+            },
+            /** The content displayed below the resizable box and above the button. */
+            prompt: {
+                type: jsPsych.ParameterType.HTML_STRING,
+                pretty_name: "Prompt",
+                default: null,
+            },
+            /** The initial size of the box, in pixels, along the larget dimension. */
+            starting_size: {
+                type: jsPsych.ParameterType.INT,
+                pretty_name: "Starting size",
+                default: 100,
+            },
+            /** Label to display on the button to complete calibration. */
+            button_label: {
+                type: jsPsych.ParameterType.STRING,
+                pretty_name: "Button label",
+                default: "Continue",
+            },
+        },
+    };
+    /**
+     * **get screen size**
+     *
+     * jsPsych plugin for getting the actual screen size and pixel size
+     *
+     * @author 
+     * @see {@link # get-screen-size plugin documentation on jspsych.org}
+     */
+    class GetScreenSize {
+        constructor(jsPsych) {
+            this.jsPsych = jsPsych;
+        }
+        trial(display_element, trial) {
+            var aspect_ratio = trial.item_width / trial.item_height;
+            // variables to determine div size
+            if (trial.item_width >= trial.item_height) {
+                var start_div_width = trial.starting_size;
+                var start_div_height = Math.round(trial.starting_size / aspect_ratio);
+            }
+            else {
+                var start_div_height = trial.starting_size;
+                var start_div_width = Math.round(trial.starting_size * aspect_ratio);
+            }
+            // create html for display
+            var html = '<div id="jspsych-get-screen-size-div" style="border: 2px solid steelblue; height: ' +
+                start_div_height +
+                "px; width:" +
+                start_div_width +
+                'px; margin: 7px auto; background-color: lightsteelblue; position: relative;">';
+            html +=
+                '<div id="jspsych-get-screen-size-handle" style="cursor: nwse-resize; background-color: steelblue; width: 10px; height: 10px; border: 2px solid lightsteelblue; position: absolute; bottom: 0; right: 0;"></div>';
+            html += "</div>";
+            if (trial.prompt !== null) {
+                html += trial.prompt;
+            }
+            html += '<a class="jspsych-btn" id="jspsych-get-screen-size-btn">' + trial.button_label + "</a>";
+            // render
+            display_element.innerHTML = html;
+            // function to end trial
+            const end_trial = () => {
+                // clear document event listeners
+                document.removeEventListener("mousemove", resizeevent);
+                document.removeEventListener("mouseup", mouseupevent);
+                // get the pixel of the screen
+                let cal_height = document.querySelector("#jspsych-get-screen-size-div").clientHeight,
+                    cal_width = document.querySelector("#jspsych-get-screen-size-div").clientWidth,
+                    screen_width = window.screen.width,
+                    screen_height = window.screen.height;
+                // clear the screen
+                display_element.innerHTML = "";
+                // finishes trial
+                var trial_data = {
+                    screen_width_pixel: screen_width,
+                    screen_width_physics: screen_width * ((trial.item_width > trial.item_height ? trial.item_width : trial.item_height) / cal_width),
+                    screen_height_physics: screen_height * ((trial.item_width > trial.item_height ? trial.item_height : trial.item_width) / cal_height),
+                    screen_height_pixel: screen_height,
+                };
+                this.jsPsych.finishTrial(trial_data);
+            };
+            // listens for the click
+            document.getElementById("jspsych-get-screen-size-btn").addEventListener("click", () => {
+                end_trial();
+            });
+            var dragging = false;
+            var origin_x, origin_y;
+            var cx, cy;
+            var mousedownevent = (e) => {
+                e.preventDefault();
+                dragging = true;
+                origin_x = e.pageX;
+                origin_y = e.pageY;
+                cx = parseInt(scale_div.style.width);
+                cy = parseInt(scale_div.style.height);
+            };
+            display_element
+                .querySelector("#jspsych-get-screen-size-handle")
+                .addEventListener("mousedown", mousedownevent);
+            var mouseupevent = (e) => {
+                dragging = false;
+            };
+            document.addEventListener("mouseup", mouseupevent);
+            var scale_div = display_element.querySelector("#jspsych-get-screen-size-div");
+            var resizeevent = (e) => {
+                if (dragging) {
+                    var dx = e.pageX - origin_x;
+                    var dy = e.pageY - origin_y;
+                    if (Math.abs(dx) >= Math.abs(dy)) {
+                        scale_div.style.width = Math.round(Math.max(20, cx + dx * 2)) + "px";
+                        scale_div.style.height = Math.round(Math.max(20, cx + dx * 2) / aspect_ratio) + "px";
+                    }
+                    else {
+                        scale_div.style.height = Math.round(Math.max(20, cy + dy * 2)) + "px";
+                        scale_div.style.width = Math.round(aspect_ratio * Math.max(20, cy + dy * 2)) + "px";
+                    }
+                }
+            };
+            document.addEventListener("mousemove", resizeevent);
+        }
+    }
+    GetScreenSize.info = info;
+
+    return GetScreenSize;
+
+})();
